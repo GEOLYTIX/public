89814e321aac3eab7aba2dbcf8c90c7de959e0b6
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-18 12:40:51 +0100

    Create get_constituency_from_region.sql

---
+++ wellbeing/query/get_constituency_from_region.sql
@@ -0,0 +1,7 @@
+
+SELECT ARRAY(
+  SELECT constituency_name 
+  FROM coop.uk_open_admin_constituencies_regions 
+  WHERE regionname = '${region}'
+  ORDER BY constituency_name
+) AS constituency_name;
