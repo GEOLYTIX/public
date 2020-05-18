d1cd5861a6a4e1339b5fd7ac16b890df139f9aa0
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-18 12:37:35 +0100

    Create community_wellbeing_place_constituency.sql

---
+++ wellbeing/query/community_wellbeing_place_constituency.sql
@@ -0,0 +1,41 @@
+SELECT
+ id,
+ dd_name,
+ '#FFD60C' AS backgroundcolour,
+  round(cul_places_worship * 100) AS cul_places_worship,
+  round(cul_types_workers * 100) AS cul_types_workers,
+  round(cul_areas_leisure * 100) AS cul_areas_leisure,
+  round(cul_museums * 100) AS cul_museums,
+  round(cul_listed_buildings * 100) AS cul_listed_buildings,
+  round(tra_communication_internet * 100) AS tra_communication_internet,
+  round(tra_public_transport * 100) AS tra_public_transport,
+  round(hou_affordability * 100) AS hou_affordability,
+  round(hou_overcrowding * 100) AS hou_overcrowding,
+  round(hou_green_space * 100) AS hou_green_space,
+  round(hou_public_spaces * 100) AS hou_public_spaces,
+  round(hou_pollution * 100) AS hou_pollution,
+  round(hou_air_quality * 100) AS hou_air_quality
+FROM coop.uk_coop_restrict_wellbeing
+WHERE dd_name = '${loc}'
+
+UNION ALL
+
+SELECT 
+  id,
+  dd_name,
+  null AS backgroundcolour,
+  round(cul_places_worship * 100) AS cul_places_worship,
+  round(cul_types_workers * 100) AS cul_types_workers,
+  round(cul_areas_leisure * 100) AS cul_areas_leisure,
+  round(cul_museums * 100) AS cul_museums,
+  round(cul_listed_buildings * 100) AS cul_listed_buildings,
+  round(tra_communication_internet * 100) AS tra_communication_internet,
+  round(tra_public_transport * 100) AS tra_public_transport,
+  round(hou_affordability * 100) AS hou_affordability,
+  round(hou_overcrowding * 100) AS hou_overcrowding,
+  round(hou_green_space * 100) AS hou_green_space,
+  round(hou_public_spaces * 100) AS hou_public_spaces,
+  round(hou_pollution * 100) AS hou_pollution,
+  round(hou_air_quality * 100) AS hou_air_quality
+FROM coop.uk_coop_restrict_wellbeing
+WHERE constituency_name = '${constituency}';
