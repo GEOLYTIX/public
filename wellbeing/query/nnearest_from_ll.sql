eb157d9a5e31c605305b763750651006943118e6
dbauzus-glx <dennis.bauszus@geolytix.co.uk>
2020-05-18 12:42:20 +0100

    Create nnearest_from_ll.sql

---
+++ wellbeing/query/nnearest_from_ll.sql
@@ -0,0 +1,25 @@
+SELECT
+  st_asgeojson(geom_4326) AS geomj,
+  id,
+  localename,
+  dd_name,
+  region,
+  country,
+  census11pop,
+  census11dwelling,
+  urban,
+  rural,
+  area_sqm,
+  indrelat,
+  indequal,
+  indvoice,
+  indecono,
+  indhealt,
+  indeduca,
+  indcultu,
+  indhousi,
+  indtrans,
+  ind_wellb
+FROM coop.uk_coop_restrict_wellbeing
+ORDER BY geom_p_4326 <-> st_setsrid(st_point(${lng}, ${lat}), 4326)
+LIMIT ${n};
