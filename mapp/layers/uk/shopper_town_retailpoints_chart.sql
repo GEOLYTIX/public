028f891b5b48f0784ad7b63fb72764a370948d59
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-07-14 17:30:17 +0100

    retailpoint charts

--- mapp/layers/uk/shopper_town_retailpoints_chart.sql
+++ mapp/layers/uk/shopper_town_retailpoints_chart.sql
@@ -1,11 +1,12 @@
-select
- a.retailer,
- count(a.id)
-from
-    geodata.uk_glx_open_retail_points a,
-    geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-where
-    b.id = ${id}
-    and st_intersects(b.geom_4326, a.geom_p_4326)
-    and a.retailer in ('Sainsburys', 'Asda', 'Tesco', 'Morrisons', 'Waitrose', 'Lidl', 'Aldi', 'Co-op')
-group by a.retailer
+select array_agg(c.count) from (
+    select
+    a.retailer,
+    count(a.id)
+    from
+        geodata.uk_glx_open_retail_points a,
+        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
+    where
+        b.id = '2110000319'
+        and st_intersects(b.geom_4326, a.geom_p_4326)
+        and a.retailer in ('Sainsburys', 'Asda', 'Tesco', 'Morrisons', 'Waitrose', 'Lidl', 'Aldi', 'Co-op')
+    group by a.retailer) as c
