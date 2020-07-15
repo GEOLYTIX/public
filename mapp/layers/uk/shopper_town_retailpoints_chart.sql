6962f74edf95c1f47e9248f83475b0008d21091c
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-07-15 11:14:09 +0100

    shopper town query

--- mapp/layers/uk/shopper_town_retailpoints_chart.sql
+++ mapp/layers/uk/shopper_town_retailpoints_chart.sql
@@ -1,80 +1,35 @@
-select
-    (select
-    count(a.id)
-    from
-        geodata.uk_glx_open_retail_points a,
-        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-    where
-        b.id = ${id}
-        and st_intersects(b.geom_4326, a.geom_p_4326)
-        and a.retailer = 'Sainsburys') as "Sainsburys",
-
-    (select
-    count(a.id)
-    from
-        geodata.uk_glx_open_retail_points a,
-        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-    where
-        b.id = ${id}
-        and st_intersects(b.geom_4326, a.geom_p_4326)
-        and a.retailer = 'Asda') as "Asda",
-
-    (select
-    count(a.id)
-    from
-        geodata.uk_glx_open_retail_points a,
-        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-    where
-        b.id = ${id}
-        and st_intersects(b.geom_4326, a.geom_p_4326)
-        and a.retailer = 'Tesco') as "Tesco",
-
-    (select
-    count(a.id)
-    from
-        geodata.uk_glx_open_retail_points a,
-        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-    where
-        b.id = ${id}
-        and st_intersects(b.geom_4326, a.geom_p_4326)
-        and a.retailer = 'Morrisons') as "Morrisons",
-
-    (select
-    count(a.id)
-    from
-        geodata.uk_glx_open_retail_points a,
-        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-    where
-        b.id = ${id}
-        and st_intersects(b.geom_4326, a.geom_p_4326)
-        and a.retailer = 'Waitrose') as "Waitrose",
-
-    (select
-    count(a.id)
-    from
-        geodata.uk_glx_open_retail_points a,
-        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-    where
-        b.id = ${id}
-        and st_intersects(b.geom_4326, a.geom_p_4326)
-        and a.retailer = 'Lidl') as "Lidl",
-
-    (select
-    count(a.id)
-    from
-        geodata.uk_glx_open_retail_points a,
-        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-    where
-        b.id = ${id}
-        and st_intersects(b.geom_4326, a.geom_p_4326)
-        and a.retailer = 'Aldi') as "Aldi",
-
-    (select
-    count(a.id)
-    from
-        geodata.uk_glx_open_retail_points a,
-        geodata.uk_glx_geodata_seamless_shopper_town_metrics b 
-    where
-        b.id = ${id}
-        and st_intersects(b.geom_4326, a.geom_p_4326)
-        and a.retailer = 'Co-op') as "Co-op";      
+SELECT sum(case
+               when rpp.retailer='Sainsburys' then 1
+               else 0
+           end) as "Sainsburys",
+       sum(case
+               when rpp.retailer='Asda' then 1
+               else 0
+           end) as "Asda",
+       sum(case
+               when rpp.retailer='Tesco' then 1
+               else 0
+           end) as "Tesco",
+       sum(case
+               when rpp.retailer='Morrisons' then 1
+               else 0
+           end) as "Morrisons",
+       sum(case
+               when rpp.retailer='Waitrose' then 1
+               else 0
+           end) as "Waitrose",
+       sum(case
+               when rpp.retailer='Lidl' then 1
+               else 0
+           end) as "Lidl",
+       sum(case
+               when rpp.retailer='Aldi' then 1
+               else 0
+           end) as "Aldi",
+       sum(case
+               when rpp.retailer='Co-op' then 1
+               else 0
+           end) as "Co-op"
+FROM geodata.uk_glx_open_retail_points rpp
+inner join geodata.uk_glx_geodata_seamless_shopper_town_metrics sst on st_intersects(sst.geom_4326, rpp.geom_p_4326)
+where sst.id = ${id};
