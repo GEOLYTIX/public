cd3cd8a7eb22dbc7285105503ebc8c84051ab74a
Daniel Sawyers <DanielSawyers@gmail.com>
2020-07-24 15:27:44 +0100

    Added Population Trend query

--- mapp/layers/uk/sites_home_ownership.sql
+++ mapp/layers/uk/sites_home_ownership.sql
@@ -1,8 +1,8 @@
 select 
-	coalesce(sum(dwelling_ownhome) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0) as owner_occupied,
-	coalesce(sum(dwelling_privaterented) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0) as renting,
-	coalesce(sum(dwelling_socialrented) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0) as social,
-	coalesce(sum(dwelling_rentfree) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0) as rent_free
+	coalesce(sum(dwelling_ownhome) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0) as "Owner Occupied",
+	coalesce(sum(dwelling_privaterented) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0) as "Renting",
+	coalesce(sum(dwelling_socialrented) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0) as "Social",
+	coalesce(sum(dwelling_rentfree) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0) as "Rent Free"
 from mapp.uk_glx_sites sit
 left join geodata.uk_glx_geodata_oa_metrics oa
 on st_intersects(sit.isoline_15min, oa.geom_p_4326)
