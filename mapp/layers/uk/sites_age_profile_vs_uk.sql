ada4cc6af92b090692b7a57466291d6a6cdcb5f1
Daniel Sawyers <DanielSawyers@gmail.com>
2020-07-24 11:48:19 +0100

    Fix Age profile

--- mapp/layers/uk/sites_age_profile_vs_uk.sql
+++ mapp/layers/uk/sites_age_profile_vs_uk.sql
@@ -1,10 +1,10 @@
  select 
- 	dat.age0to18 / (uk.social_grade_ab_uk / cast(uk.age_under_18_uk as numeric))  *100 as age_under_18,
-	dat.age18to24 / (uk.social_grade_c1_uk / cast(uk.age_18to24_uk as numeric))  *100 as age_18_24,
-	dat.age25to29 / (uk.social_grade_c2_uk / cast(uk.age_25to29_uk as numeric))  *100 as age_25_29,
-	dat.age30to44 / (uk.social_grade_de_uk / cast(uk.age_30to44_uk as numeric))  *100 as age_30_44,
-	dat.age45to59 / (uk.social_grade_de_uk / cast(uk.age_45to59_uk as numeric))  *100 as age_45_59,
-	dat.age60plus / (uk.social_grade_de_uk / cast(uk.age_60plus_uk as numeric))  *100 as age_over_60
+ 	dat.age0to18 / (uk.age_under_18_uk / cast(uk.age_total_uk as numeric))  *100 as age_under_18,
+	dat.age18to24 / (uk.age_18to24_uk / cast(uk.age_total_uk as numeric))  *100 as age_18_24,
+	dat.age25to29 / (uk.age_25to29_uk / cast(uk.age_total_uk as numeric))  *100 as age_25_29,
+	dat.age30to44 / (uk.age_30to44_uk / cast(uk.age_total_uk as numeric))  *100 as age_30_44,
+	dat.age45to59 / (uk.age_45to59_uk / cast(uk.age_total_uk as numeric))  *100 as age_45_59,
+	dat.age60plus / (uk.age_60plus_uk / cast(uk.age_total_uk as numeric))  *100 as age_over_60
 from 
  (select  
  	COALESCE(sum(age0to18) / nullif(cast(sum(age0to18 +age18to24 +age25to29 + age30to44 + age45to59 + age60plus) as numeric),0),0) as age0to18,
