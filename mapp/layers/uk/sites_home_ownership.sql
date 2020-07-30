ec0858b499ac7e8b5f7879f896fe3045e34025c0
Daniel Sawyers <DanielSawyers@gmail.com>
2020-07-30 19:21:02 +0100

    Home ownership rounding fix

--- mapp/layers/uk/sites_home_ownership.sql
+++ mapp/layers/uk/sites_home_ownership.sql
@@ -1,7 +1,10 @@
-select round(coalesce(sum(dwelling_ownhome) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0),2) as "Owner Occupied",
-       round(coalesce(sum(dwelling_privaterented) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0),2) as "Renting",
-       round(coalesce(sum(dwelling_socialrented) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0),2) as "Social",
-       round(coalesce(sum(dwelling_rentfree) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0),2) as "Rent Free"
+select 
+	round(100* coalesce(sum(dwelling_ownhome) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0),2) as "Owner Occupied",
+	round(100* coalesce(sum(dwelling_privaterented) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0),2) as "Renting",
+	round(100* coalesce(sum(dwelling_socialrented) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0),2) as "Social",
+	round(100* coalesce(sum(dwelling_rentfree) / nullif(cast(sum(dwelling_ownhome+dwelling_privaterented+dwelling_socialrented+dwelling_rentfree) as numeric),0),0),2) as "Rent Free"
 from mapp.uk_glx_sites sit
-left join geodata.uk_glx_geodata_oa_metrics oa on st_intersects(sit.isoline_15min, oa.geom_p_4326)
+left join geodata.uk_glx_geodata_oa_metrics oa
+on st_intersects(sit.isoline_15min, oa.geom_p_4326)
 where sit.id=${id};
+
