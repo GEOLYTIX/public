49ab5a72f15392daa7e5a698b790614ec00e347c
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-08-21 17:41:33 +0100

    sites competition

--- mapp/layers/uk/sites_stores_supermarkets.sql
+++ mapp/layers/uk/sites_stores_supermarkets.sql
@@ -1,23 +1,17 @@
 select 
-	sto.store_name as "Name",
-	sto.fascia as "Fascia",
-  	round(cast(st_distance(sit.geom_4326, sto.geom_p_4326, true) *0.000621371 as numeric),2) as "Distance (mi)",
-	case size_band
-		when '30,138 ft2 > (2,800 m2)' then '>30k'
-		when '15,069 < 30,138 ft2 (1,400 < 2,800 m2)'  then '15-30k'
-		when '< 3,013 ft2 (280m2)' then '<3k'
-		when '3,013 < 15,069 ft2 (280 < 1,400 m2)' then '3-15k'
-	end as "Size (sq ft)",
+	sto.store_name,
+	sto.fascia,
+    sto.size_sqft,
+  	round(cast(st_distance(sit.geom_4326, sto.geom_p_4326, true) *0.000621371 as numeric),2) as "distance",
 	case 
 		when st_intersects(sit.isoline_5min, sto.geom_p_4326) then '0-5 mins' 
 		when st_intersects(sit.isoline_10min, sto.geom_p_4326) then '5-10 mins' 
 		when st_intersects(sit.isoline_15min, sto.geom_p_4326) then '10-15 mins' 
-	end as "Dt Band"
+	end as "band"
 from mapp.uk_glx_sites sit
 left join geodata.uk_glx_open_retail_points sto
 on st_intersects(sit.isoline_15min, sto.geom_p_4326)
+and sto.store_type = 'Supermarket'
 where sit.id =  ${id}
 and sto.size_band <>'< 3,013 ft2 (280m2)'
-order by st_distance(sit.geom_4326, sto.geom_p_4326, true)
-limit 10;
-
+order by st_distance(sit.geom_4326, sto.geom_p_4326, true);
