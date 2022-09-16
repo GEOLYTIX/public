864e74b9626280fbe0f666830b46fee313b005c2
Agata Brok <cityremade@users.noreply.github.com>
2022-09-16 18:15:31 +0200

    Summer 2022/

--- summer_2022/index.js
+++ summer_2022/index.js
@@ -26,37 +26,65 @@ window.onload = async () => {
     createStories()
 
     function createStories(){
-        
-        mapp.utils.xhr({
-            url: 'https://geolytix.github.io/public/summer_2022/data.json'
-        }).then(data => {
 
+        const xhr = new XMLHttpRequest()
+
+        xhr.open('GET', 'https://geolytix.github.io/public/summer_2022/data.json')
+
+        xhr.onload = e => {
             if(!e.target) return;
-
             if(!e.target.response) return;
 
-            const json = JSON.parse(e.target.response)
-
             document.getElementById('stories').innerHTML = ''
+
             state.els = null
 
-            if(!state.stories) state.stories = Object.values(json)
-            
-            stories.map((story, i) => {
-                console.log({
-                    story: story,
-                    i: i
+            if(!state.stories) state.stories = JSON.parse(e.target.response)
+
+            Object.values(state.stories).map((story, i) => {
+
+                let el = mapp.utils.html.node`<div class="story" data-lat=${story.lat} data-lng=${story.lng}>`
+
+                let div = mapp.utils.html.node`<div>`
+
+                story.images.map((image, j) => {
+                    let img = mapp.utils.html.node`<img src="${image}" class="${j%2 === 0 ? `bottom` : `top`}">`
+                    div.appendChild(img)
                 })
+
+                let card = mapp.utils.html.node`<div class="card">
+                <h4>${story.place}</h4>
+                <h2>${story.title}</h2>
+                <p>${story.description}</p>
+                <h3>${story.name}`
+
+                el.appendChild(div)
+
+                el.appendChild(card)
+                
+                document.getElementById('stories').appendChild(el)
+            
             })
 
+            state.els = Array.from(document.querySelectorAll('#stories div.story'))
+            let elm = state.els.find(el => isInFocus(el)) // initialize map view
+            let idx = state.els.indexOf(elm)
+            state.prev = state.current
+            state.current = idx < 0 ? 0 : idx
 
-        })
+            let dataset = state.els[state.current].dataset
+            
+            setView({
+                lat: parseFloat(dataset.lat),
+                lng: parseFloat(dataset.lng)
+            })
+        }
+
+        xhr.send()
+
     }
 
-    //temporary
-    //state.els = Array.from(document.querySelectorAll('#stories div'))
-
-    /*document.addEventListener('scroll', () => {
+    document.addEventListener('scroll', () => {
 
         const el = state.els.find(el => isInFocus(el))
 
@@ -68,23 +96,48 @@ window.onload = async () => {
 
         if(state.current === state.prev) {
             //no moving
-            console.log('no moving')
         } else {
             if(!el) return
-            console.log(el.outerHTML)
-            //if(!state.els[idx]?.dataset?.story) return
-            //let view = JSON.parse(state.els[idx].dataset.story)
-            //setView(view)
+            let dataset = state.els[idx].dataset;
+            if(!dataset) return;
+            
+            setView({
+                lat: parseFloat(dataset.lat),
+                lng: parseFloat(dataset.lng)
+            })
         }
 
-    }, { passive: true })*/
+    }, { passive: true })
 
     function isInFocus(el) {
         const rect = el.getBoundingClientRect()
         return rect.top > 0 && rect.top < window.innerHeight/3
     }
 
-    function setView(story) {}
+    function setView(location) {
+        if(state.layer) map.Map.removeLayer(state.layer)
+        if(!location.pin) location.pin = "https://geolytix.github.io/public/summer_2022/postcard_pin.svg"
+
+        let pnt = new ol.Feature({
+            geometry: new ol.geom.Point([location.lng, location.lat]).transform('EPSG:4326', 'EPSG:'+ map.srid)
+        })
+        const source = new ol.source.Vector()
+        source.addFeatures([pnt])
+
+        state.layer = new ol.layer.Vector({
+            source: source,
+            style: mapp.utils.style({
+                icon: {
+                    svg: location.pin,
+                    scale: 2
+                }
+            })
+        })
+
+        map.Map.addLayer(state.layer)
+
+        map.fitView(source.getExtent())
+    }
 
 
 
