bdc36fca22c102fc8099b21a8b74a1935bfc6522
Agata Brok <cityremade@users.noreply.github.com>
2022-09-16 09:44:29 +0200

    Summer 2022

--- summer_2022/index.js
+++ summer_2022/index.js
@@ -1,32 +1,62 @@
 window.onload = async () => {
-	
-	const host = document.head.dataset.dir
+    
+    const host = document.head.dataset.dir
 
-	const locale = await mapp.utils.xhr(`${host}/api/workspace/locale?locale=summer`)
+    const locale = await mapp.utils.xhr(`${host}/api/workspace/locale?locale=summer`)
 
-	const layers = await mapp.utils.promiseAll(
-		Array.from(["mapbox"]).map(layer => mapp.utils.xhr(`${host}/api/workspace/layer?locale=${locale.key}&layer=${layer}`))
+    const layers = await mapp.utils.promiseAll(
+        Array.from(["mapbox"]).map(layer => mapp.utils.xhr(`${host}/api/workspace/layer?locale=${locale.key}&layer=${layer}`))
     )
 
     const state = {}
 
-	const map = mapp.Mapview({
-		host: host,
+    const map = mapp.Mapview({
+        host: host,
         target: "map",
         locale: locale,
         scrollWheelZoom: true
     })
 
     await map.addLayer(
-    	layers.map(l => {
-    		l.display = true
-    		return l
+        layers.map(l => {
+            l.display = true
+            return l
     }))
 
-    //temporary
-    state.els = Array.from(document.querySelectorAll('#stories div'))
+    createStories()
 
-    document.addEventListener('scroll', () => {
+    function createStories(){
+        
+        mapp.utils.xhr({
+            url: 'https://geolytix.github.io/public/summer_2022/data.json'
+        }).then(data => {
+
+            if(!e.target) return;
+
+            if(!e.target.response) return;
+
+            const json = JSON.parse(e.target.response)
+
+            document.getElementById('stories').innerHTML = ''
+            state.els = null
+
+            if(!state.stories) state.stories = Object.values(json)
+            
+            stories.map((story, i) => {
+                console.log({
+                    story: story,
+                    i: i
+                })
+            })
+
+
+        })
+    }
+
+    //temporary
+    //state.els = Array.from(document.querySelectorAll('#stories div'))
+
+    /*document.addEventListener('scroll', () => {
 
         const el = state.els.find(el => isInFocus(el))
 
@@ -43,17 +73,18 @@ window.onload = async () => {
             if(!el) return
             console.log(el.outerHTML)
             //if(!state.els[idx]?.dataset?.story) return
-            
             //let view = JSON.parse(state.els[idx].dataset.story)
             //setView(view)
         }
 
-    }, { passive: true })
+    }, { passive: true })*/
 
     function isInFocus(el) {
-    	const rect = el.getBoundingClientRect()
+        const rect = el.getBoundingClientRect()
         return rect.top > 0 && rect.top < window.innerHeight/3
     }
+
+    function setView(story) {}
 
 
 
